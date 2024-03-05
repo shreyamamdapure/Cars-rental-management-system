a8875d18b32b6e07f462dca7484ec8c2cb9a992b
VISTEON\SRAJSHEK <shreya.mamdapure@gmail.com>
2024-03-05 18:11:39 +0530

    Created a new branch and setup a basic cpp eclipse project

---
+++ source/CarRentalManagement/src/CarRentalManagement.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CarRentalManagement.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Welcome to car rental management system" << endl; // prints Welcome to car rental management system
+	return 0;
+}
