5ebd2120f074b04f03f3a26e9a6b16e8236161be
Nithish V <nithish2003v@gmail.com>
2024-03-11 13:21:59 +0530

    Initial version of reports WIP

---
+++ source/CarRentalManagement/src/report_display.h
@@ -0,0 +1,7 @@
+#include <vector>
+
+void displayCars(const vector<Car>& cars);
+void displayRentedCars(const vector<Car>& RentedCars);
+void displaySubscribers(const vector<Subscriber> subscribers);
+void displayCurrentSubscription(const vector<Car>& RentedCars);
+void displayRevenue(const vector<Car>& RentedCars);
